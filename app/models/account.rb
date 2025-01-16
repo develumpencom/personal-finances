60618075ae2d98282b944cd24f8596e803069031
develumpen <develumpen@breakabletoys.com>
2025-01-15 18:01:16 -0600

    add import settings to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,14 @@
 class Account < ApplicationRecord
   has_many :movements, dependent: :destroy
 
+  SETTINGS_FIELDS = [ :transaction_date_column,
+                      :transaction_date_format,
+                      :post_date_column,
+                      :post_date_format,
+                      :description_column,
+                      :amount_column ]
+
+  store :import_settings, accessors: [ *SETTINGS_FIELDS ]
+
   validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_12_194947) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_15_233227) do
   create_table "accounts", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "import_settings"
   end
 
   create_table "movements", force: :cascade do |t|

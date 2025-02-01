20ea3e4a9c200ff7addc1824411795985cf83970
develumpencom <develumpen@breakabletoys.com>
2025-01-31 19:53:25 -0600

    extract flashes into common partial (#17)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_classes(flash_type)
+    "py-2 px-3 mb-5 font-medium rounded-lg inline-block " +
+
+    class_names({
+      "bg-green-50 text-green-500": flash_type == "notice",
+      "bg-red-50 text-red-500": flash_type == "alert"
+    })
+  end
 end
